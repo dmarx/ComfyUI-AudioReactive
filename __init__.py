fbf8388437245f4594c5ef87582a4278fdfd4391
David Marx <david.marx84@gmail.com>
2023-12-31 22:06:16 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,36 @@
 from nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
+
+import os
+import subprocess
+import importlib.util
+import sys
+
+# scavenged install sequence from https://github.com/FizzleDorf/ComfyUI_FizzNodes/blob/main/__init__.py
+def is_installed(package, package_overwrite=None):
+    try:
+        spec = importlib.util.find_spec(package)
+    except ModuleNotFoundError:
+        pass
+
+    package = package_overwrite or package
+
+    python = sys.executable
+    if spec is None:
+        print(f"Installing {package}...")
+        command = f'"{python}" -m pip install {package}'
+  
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, env=os.environ)
+
+        if result.returncode != 0:
+            print(f"Couldn't install\nCommand: {command}\nError code: {result.returncode}")
+
+# to do: read from requirements.txt
+is_installed("scipy")
+is_installed("scikit-learn")
+is_installed("librosa")
+
+
+print(os.environ.get('COMFYUI_DEBUG_MODE'))
+
