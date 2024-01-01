d4094d33e2def5bd1ee2caafbf299ab303aa30df
David Marx <david.marx84@gmail.com>
2024-01-01 11:52:58 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 #from nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-from .nodes.audio_nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+#from .nodes.audio_nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 from loguru import logger
 
